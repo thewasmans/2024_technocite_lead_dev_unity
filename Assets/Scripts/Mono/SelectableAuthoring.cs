001f7e48f45871e810e60f1d8013dd45ade3571c
thewasmans <thewasmans@gmail.Com>
2024-09-18 14:40:25 +0200

    save exercice

--- Assets/Scripts/Mono/SelectableAuthoring.cs
+++ Assets/Scripts/Mono/SelectableAuthoring.cs
@@ -1,10 +1,17 @@
-public class SelectableAuthoring
+using Unity.Entities;
+using UnityEngine;
+
+public class SelectableAuthoring : MonoBehaviour
 {
-    // private class Baker : Baker<SelectableAuthoring>
-    // {
-    //     public override void Bake(SelectableAuthoring authoring)
-    //     {
-            
-    //     }
-    // }
+    private class Baker : Baker<SelectableAuthoring>
+    {
+        public override void Bake(SelectableAuthoring authoring)
+        {
+            //var entities = World.DefaultGameObjectInjectionWorld.EntityManager.CreateEntityQuery<RefRO<Selectable>>();
+            // foreach (var (entity) in entities)
+            // {
+                
+            // }
+        }
+    }
 }
