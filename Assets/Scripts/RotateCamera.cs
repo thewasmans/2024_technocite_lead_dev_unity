580b53daa0f9239a7210fb2e28ff32ec1c202c38
thewasmans <thewasmans@gmail.Com>
2024-10-14 17:27:55 +0200

    Rename Crew To rotate Camera

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class crew : MonoBehaviour
+public class RotateCamera : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
